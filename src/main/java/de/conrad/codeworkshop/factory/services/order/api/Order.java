6eee51176649636280e339005090b5bfb4d795d4
RomeuFranzoiaJr <rfranzoia@gmail.com>
2020-06-03 11:01:51 +0100

    extended order validations

--- src/main/java/de/conrad/codeworkshop/factory/services/order/api/Order.java
+++ src/main/java/de/conrad/codeworkshop/factory/services/order/api/Order.java
@@ -1,8 +1,11 @@
 package de.conrad.codeworkshop.factory.services.order.api;
 
-import java.util.List;
+import static de.conrad.codeworkshop.factory.services.order.api.OrderStatus.ACCEPTED;
+import static de.conrad.codeworkshop.factory.services.order.api.OrderStatus.DECLINED;
+import static de.conrad.codeworkshop.factory.services.order.api.OrderStatus.PENDING;
 
-import static de.conrad.codeworkshop.factory.services.order.api.OrderStatus.*;
+import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * @author Andreas Hartmann
@@ -13,13 +16,36 @@ public class Order {
     private OrderStatus status = PENDING;
 
     public void validate() {
-        if (!positions.isEmpty() && status == PENDING) {
+        if (!positions.isEmpty() && arePositionsValid() && status == PENDING) {
             status = ACCEPTED;
         } else {
             status = DECLINED;
         }
     }
 
+    public boolean arePositionsValid() {
+    	
+    	final Predicate<Position> validateProductIdSize = position -> {
+    		final int size = position.getProductId().toString().length();
+    		return (size >= 6 && size <= 9);
+    	};
+    	
+    	
+    	final Predicate<Position> validateQuantityIsDivisibleByTen = position -> 
+    					position.getQuantity().doubleValue() % 10 == 0;
+    	
+    	final Predicate<Position> validateLessOneGreaterZero = position -> 
+    					position.getQuantity().doubleValue() > 0.0 && position.getQuantity().doubleValue() < 1.0;
+    					
+		final double fixedValue = 42.42;
+    	Predicate<Position> validateFixedValue = position -> position.getQuantity().doubleValue() == fixedValue;
+    	
+    	return positions.stream().allMatch(validateProductIdSize
+    										.and(validateQuantityIsDivisibleByTen
+													.or(validateLessOneGreaterZero)
+													.or(validateFixedValue)));
+    }
+    
     public void setOrderConfirmation(final OrderConfirmation orderConfirmation) {
         this.orderConfirmation = orderConfirmation;
     }
@@ -39,4 +65,5 @@ public class Order {
     public void setStatus(OrderStatus status) {
         this.status = status;
     }
+    
 }
