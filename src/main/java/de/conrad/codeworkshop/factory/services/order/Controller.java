9e1ff3945c6ac2d455155228a5a0e3a42bf7fa8e
RomeuFranzoiaJr <rfranzoia@gmail.com>
2020-06-03 11:03:36 +0100

    changed controller to accept json

--- src/main/java/de/conrad/codeworkshop/factory/services/order/Controller.java
+++ src/main/java/de/conrad/codeworkshop/factory/services/order/Controller.java
@@ -3,7 +3,9 @@ package de.conrad.codeworkshop.factory.services.order;
 import de.conrad.codeworkshop.factory.services.order.api.Order;
 import de.conrad.codeworkshop.factory.services.order.api.OrderConfirmation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +23,8 @@ public class Controller {
         this.factoryService = service;
     }
 
-    @PostMapping("/create")
-    public OrderConfirmation createOrder(final Order order) {
+    @PostMapping(path = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public OrderConfirmation createOrder(@RequestBody final Order order) {
         return factoryService.createOrder(order);
     }
 }
